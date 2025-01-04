3bb7a9fec4559ddc789c424b92a92122ad09c1f4
Gregg Tavares <github@greggman.com>
2025-01-04 14:49:41 -0800

    copy from Debug

--- build/build.js
+++ build/build.js
@@ -73,7 +73,7 @@ async function packageExtension(target) {
 }
 
 async function copyResult(filepath, target) {
-  const srcFilename = path.join(filepath, 'dawn.node');
+  const srcFilename = path.join(...[filepath, ...addElemIf(isWin, 'Debug'), 'dawn.node']);
   const dstFilename = path.join('dist', `${target}.dawn.node`);
   fs.mkdirSync(path.dirname(dstFilename), {recursive: true});
   fs.copyFileSync(srcFilename, dstFilename);
