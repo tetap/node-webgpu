700a1d50fd5fc1ba11b0ce793aa24d7416de0903
Gregg Tavares <github@greggman.com>
2025-01-15 10:25:48 -0800

    update update to fully update

--- build/update.js
+++ build/update.js
@@ -1,15 +1,25 @@
+import path from 'node:path';
+
 import {execute} from './execute.js';
-    
+import {prependPathIfItExists} from './utils.js';
+
 const cwd = process.cwd(); 
+const depotToolsPath = path.join(cwd, 'third_party', 'depot_tools');
+prependPathIfItExists(depotToolsPath);
 
 async function main() {
   try {
     await execute('git', ['submodule', 'update', '--init']);
-    process.chdir('third_party/dawn');
-    await execute('git', ['pull', 'origin', 'main']);
-    process.chdir(cwd);
+
     process.chdir('third_party/depot_tools');
     await execute('git', ['pull', 'origin', 'main']);
+    process.chdir(cwd);
+
+    process.chdir('third_party/dawn');
+    await execute('git', ['pull', 'origin', 'main']);
+    await execute('gclient', ['sync', '-D']);
+    process.chdir(cwd);
+
    } catch (e) {
     console.error(e);
     console.error(e.stack);
