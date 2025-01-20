52882692ad5120b9e98694a71c95466d2aeb57ad
Gregg Tavares <github@greggman.com>
2025-01-19 23:09:39 -0800

    update types as part of update

--- build/update.js
+++ build/update.js
@@ -20,6 +20,8 @@ async function main() {
     await execute('gclient', ['sync', '-D']);
     process.chdir(cwd);
 
+    await execute('npm', ['install', '--save', '@webgpu/types@latest']);
+
    } catch (e) {
     console.error(e);
     console.error(e.stack);
